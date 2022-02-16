bd6e9bbb1a71036693c0097ede3bc45aca09efc4
Kazuki <kazuki.hirata@sun-asterisk.com>
2022-02-16 17:01:03 +0700

    [React]React-Router-DOM設定 #3

--- src/index.js
+++ src/index.js
@@ -7,9 +7,15 @@ import reportWebVitals from './reportWebVitals';
 import { store } from "./app/store";
 import { Provider } from "react-redux";
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
